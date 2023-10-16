8c711e3a54f6ee9ec32c72df2a8b9abe6bd7f8a3
Pavan Mantha <manthapavankumar11@gmail.com>
2023-10-17 03:11:42 +0530

    modified the code to analyse and translate the text

--- 05-analyze-text/Python/text-analysis/text-analysis.py
+++ 05-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 
 def main():
@@ -12,10 +14,12 @@ def main():
         cog_key = os.getenv('COG_SERVICE_KEY')
 
         # Create client using endpoint and key
+        azureKeyCredential = AzureKeyCredential(cog_key)
+        cogClient = TextAnalyticsClient(endpoint=cog_endpoint, credential=azureKeyCredential)
 
 
         # Analyze each text file in the reviews folder
-        reviews_folder = 'reviews'
+        reviews_folder = '/Users/pavanmantha/Pavans/ai102-fork/05-analyze-text/Python/text-analysis/reviews'
         for file_name in os.listdir(reviews_folder):
             # Read the file contents
             print('\n-------------\n' + file_name)
@@ -23,18 +27,33 @@ def main():
             print('\n' + text)
 
             # Get language
-
+            detectedLanguage = cogClient.detect_language(documents=[text])[0]
+            print(detectedLanguage.primary_language.name)
 
             # Get sentiment
-
+            sentimentAnalysis = cogClient.analyze_sentiment(documents=[text])[0]
+            print(f"Sentiment: {sentimentAnalysis.sentiment}")
 
             # Get key phrases
-
+            phrases = cogClient.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\n Key Phrases:")
+                for phrase in phrases:
+                    print("\t{}".format(phrase))
 
             # Get entities
-
+            entities = cogClient.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
             # Get linked entities
+            entities = cogClient.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nLinks")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
 
 
--- 06-translate-text/Python/text-translation/text-translation.py
+++ 06-translate-text/Python/text-translation/text-translation.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
-import requests, json
+import requests
+import json
+
 
 def main():
     global translator_endpoint
@@ -12,46 +14,97 @@ def main():
         load_dotenv()
         cog_key = os.getenv('COG_SERVICE_KEY')
         cog_region = os.getenv('COG_SERVICE_REGION')
-        translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
+        translator_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
 
         # Analyze each text file in the reviews folder
-        reviews_folder = 'reviews'
+        reviews_folder = '/Users/pavanmantha/Pavans/ai102-fork/06-translate-text/Python/text-translation/reviews'
         for file_name in os.listdir(reviews_folder):
             # Read the file contents
             print('\n-------------\n' + file_name)
-            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            text = open(os.path.join(reviews_folder, file_name),
+                        encoding='utf8').read()
             print('\n' + text)
 
             # Detect the language
             language = GetLanguage(text)
-            print('Language:',language)
+            print('Language:', language)
 
             # Translate if not already English
             if language != 'en':
                 translation = Translate(text, language)
                 print("\nTranslation:\n{}".format(translation))
-                
+
     except Exception as ex:
         print(ex)
 
+
 def GetLanguage(text):
-    # Default language is English
-    language = 'en'
 
-    # Use the Translator detect function
+    # Use the Azure AI Translator detect function
+    path = '/detect'
+    url = translator_endpoint + path
 
+    # Build the request
+    params = {
+        'api-version': '3.0'
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get language
+    print(response)
+    language = response[0]["language"]
 
     # Return the language
     return language
 
+
 def Translate(text, source_language):
     translation = ''
 
-    # Use the Translator translate function
+    # Use the Azure AI Translator translate function
+    path = '/translate'
+    url = translator_endpoint + path
 
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['en']
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
 
     # Return the translation
     return translation
+
 
 if __name__ == "__main__":
     main()
